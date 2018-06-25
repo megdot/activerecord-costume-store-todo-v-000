1116ab1977084e96a82242aea9eba2ae35e1147d
Megan Dotto <megan.dotto@gmail.com>
2018-06-25 03:10:27 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :business_status
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,16 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.integer "costume_inventory"
     t.integer "number_of_employees"
-    t.string "business_statis"
+    t.boolean "business_status"
     t.datetime "opening_time"
     t.datetime "closing_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.float "price"
     t.string "size"
     t.string "image_url"
     t.datetime "created_at", null: false
@@ -40,8 +42,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.text "opening_date"
     t.text "closing_date"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
